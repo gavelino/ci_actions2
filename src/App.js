0decced37ed258bdfc7c3d36ebefe69962af703e
Guilherme Avelino <gavelino@gmail.com>
2021-06-01 14:35:58 -0300

    fixing deploy 9

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My React App - Guilherme Avelino
+          My React App - Guilherme Avelino 2
         </p>
         <a
           className="App-link"
